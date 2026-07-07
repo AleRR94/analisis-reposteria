e8a1ee5504d2fbe8dd88a981a58cbaee913db866
AleRR94 <alerdzreynoso@gmail.com>
2026-07-07 14:17:47 -0600

    Fix comments in SQL semáforo logic

--- sql_queries/AD_05_semaforo_recetas.sql
+++ sql_queries/AD_05_semaforo_recetas.sql
@@ -32,7 +32,7 @@ SELECT
     
     -- Semáforo de prodcutos, se toma en cuenta el Margen y las unidades vendidas
     CASE 
-        -- Color rojo: Margen de Utilidad Bruta <= 45% o ventas menores de la mitad del promedio de ventas
+        -- Color rojo: Margen de Utilidad Bruta < 45% o ventas menores de la mitad del promedio de ventas
         WHEN Margen_UB < 45.00 OR Cantidad_Vendida < (Promedio_Unidades_Año * 0.5) 
             THEN '🔴 Crítico (Revisar Costos o Rotación)'
             
@@ -40,7 +40,7 @@ SELECT
         WHEN Margen_UB >= 60.00 AND Cantidad_Vendida >= Promedio_Unidades_Año 
             THEN '🟢 Excelente (Alta Rentabilidad y Ventas)'
             
-        -- Color amarillo: Margen de UB >45% y ventas mayores al promedio de ventas o a la mitad del promedio de ventas, 
+        -- Color amarillo: Margen de UB >45% y ventas mayores a la mitad del promedio de ventas o más, 
 		-- o bien, margen >60% pero con ventas menores al promedio de ventas pero mayores a la mitad del promedio de ventas
         ELSE '🟡 Estable / Alerta (Monitorear Estrategia)'
     END AS Semaforo_Estado
